62eabee452128a662a3688214a3c2f2190903c15
Farzi <jfarzanfar@inspirato.com>
2021-08-08 14:37:46 -0600

    Filtering and searching

--- frontend/src/app/githubtable/githubtable.component.ts
+++ frontend/src/app/githubtable/githubtable.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {GithubService} from "../github.service";
 import {Githubrepo} from "../interfaces/githubrepo";
 import {MatSort} from "@angular/material/sort";
@@ -10,18 +10,36 @@ import {MatPaginator} from "@angular/material/paginator";
   templateUrl: './githubtable.component.html',
   styleUrls: ['./githubtable.component.scss']
 })
-export class GithubtableComponent{
+export class GithubtableComponent {
+  dataSourceFull: MatTableDataSource<Githubrepo>;
   dataSource: MatTableDataSource<Githubrepo>;
   displayedColumns: string[] = ['user_name', 'repo_name', 'size'];
+  searchCriteria: string
 
   constructor(private githubService: GithubService) {
     this.githubService.getData().subscribe(d => {
-      this.dataSource = new MatTableDataSource(d);
+      this.dataSourceFull = new MatTableDataSource(d);
+      this.dataSourceFull.sort = this.sort;
+      this.dataSourceFull.paginator = this.paginator;
+      this.dataSource = this.dataSourceFull;
+    });
+  }
+
+  filter(evnt: any) {
+    if (evnt == '') {
+      this.dataSource = this.dataSourceFull;
+    } else {
+      this.dataSource = new MatTableDataSource(this.dataSource.data.filter(x => filterFunc(x, evnt)));
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-    });
+    }
   }
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
+}
+
+function filterFunc(ghr: Githubrepo, text: string) {
+  let lowerText = text.toLowerCase();
+  return ghr.repo_name.toLowerCase().includes(lowerText) || ghr.user_name.toLowerCase().includes(lowerText) || ghr.size.toString().toLowerCase().includes(lowerText)
 }
